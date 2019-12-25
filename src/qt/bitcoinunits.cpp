f1a21c7cd3839d247b5de752cf33cf7f48d2cd6b
Haiping Wu <371288107@qq.com>
2019-12-25 15:07:16 +0800

    LTC to LMC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("LMC");
+    case mBTC: return QString("mLMC");
+    case uBTC: return QString::fromUtf8("μLMC");
     default: return QString("???");
     }
 }
