5074fc1850860f229ecfc4766c7447d41fab1d93
Haiping Wu <371288107@qq.com>
2019-12-25 15:36:04 +0800

    Litecoin to Lemoncoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Lemoncoins");
+    case mBTC: return QString("Milli-Lemoncoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Lemoncoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
